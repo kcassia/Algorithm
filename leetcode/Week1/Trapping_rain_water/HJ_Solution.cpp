f2909ab44d213d76330e0e53978f02e7faf9a192
hj-d <81826971+hj-d@users.noreply.github.com>
2021-05-08 01:10:54 +0900

    Solve leetcode week1 problem (#5)
    I solved Three_sum, Trapping_rain_water and Valid_parentheses.
    Valid_parentheses problem is stack problem.
    Threem_sum is 2 point move?...
    Trapping_rain_water is dp...
    
    Signed-off-by: YiHyunJin <hj0412.yi@samsung.com>
    
    Co-authored-by: YiHyunJin <hj0412.yi@samsung.com>

--- leetcode/Week1/Trapping_rain_water/HJ_Solution.cpp
+++ leetcode/Week1/Trapping_rain_water/HJ_Solution.cpp
@@ -1,6 +1,30 @@
+#include<vector>
+
+using namespace std;
+#define Min(a,b) (a < b ? a : b)
+
+int DP[30001];
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
+        int len = height.size();
+        int max = 0;
+        int ans = 0;
+        for (int i = len - 1; i >= 0; --i) {
+            if(max < height[i]) {
+                max = height[i];
+            }
+            DP[i] = max;
+        }
+        max = 0;
+        int min = 0;
+        for (int i = 0; i < len; ++i) {
+            if(max < height[i]) {
+                max = height[i];
+            }
+            min = Min(DP[i], max);
+            ans += (min - height[i]);
+        }
+        return ans;
     }
 };
