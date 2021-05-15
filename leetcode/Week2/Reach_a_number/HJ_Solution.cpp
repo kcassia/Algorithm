ba1a83c2ce467efd06468c841a30e039a76e4ce2
hj-d <81826971+hj-d@users.noreply.github.com>
2021-05-15 16:37:47 +0900

    Solve week 2 problem (#11)
    I solved coin_change. It is DP problem.
    Four_sum problem is 2 pointer problem.
    last problem Reach a number is .... I don't know..
    Just 1, 3, 6, 10 ...... good...
    
    Signed-off-by: YiHyunJin <hj0412.yi@samsung.com>
    
    Co-authored-by: YiHyunJin <hj0412.yi@samsung.com>

--- leetcode/Week2/Reach_a_number/HJ_Solution.cpp
+++ leetcode/Week2/Reach_a_number/HJ_Solution.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int reachNumber(int target) {
-        
+        int sum = 0;
+        if (target < 0) {
+            target = -target;
+        }
+        int i = 0;
+        while(sum < target) {
+            sum += (++i);
+        }
+        if (sum == target) {
+            return i;
+        } else {
+            if ((sum & 1) ^ (target & 1)) {
+                sum += (++i);
+                return i + ((sum & 1) ^ (target & 1));
+            } else {
+                return i;   
+            }
+        }
     }
 };
